8c7226bc54e09239b13f266aa943b8327b482c0b
varshneylove <lovevarshney7002@gmail.com>
2026-01-17 16:37:19 +0530

    cors error

--- index.js
+++ index.js
@@ -10,6 +10,32 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const allowedOrigins = [
+  "https://fire-detection-system-one.vercel.app",
+  "http://localhost:5173",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type"],
+    credentials: false,
+  })
+);
+
+app.options("*", cors());
+
+
 app.use("/api/fires", firesRoutes);
 
 app.get("/api/health", (req, res) => {
